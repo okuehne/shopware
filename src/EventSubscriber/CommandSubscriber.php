519d6dbacf83529ec84fee5b755fed5b08f77539
Oliver Kühne <olli.kuehne@gmail.com>
2024-03-01 19:36:03 +0100

    feat: add Subscriber and Extension

---
+++ src/EventSubscriber/CommandSubscriber.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Okuehne\Shopware\EventSubscriber;
+
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class CommandSubscriber implements EventSubscriberInterface
+{
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ConsoleCommandEvent::class => 'onConsoleCommand',
+        ];
+    }
+
+    public function onConsoleCommand(): void
+    {
+        dd('event');
+    }
+}
